2974fd64f105a3eec3a7eecdd22ef727ec9dc5e9
GustavoAOliveira <gustavoaquino001@hotmail.com>
2023-08-17 21:48:40 -0300

    Add Exception Handler

---
+++ src/main/java/med/voll/api/exception/Exception.java
@@ -0,0 +1,41 @@
+package med.voll.api.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class Exception {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404(){
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity error400(MethodArgumentNotValidException ex){
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosErroValidacao::new).toList());
+    }
+    private record DadosErroValidacao(String campo, String mensagem){
+        private DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
